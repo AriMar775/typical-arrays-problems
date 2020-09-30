92e2aebb1acdf40a9f48984e0dd12f142e956f02
Arina Marchenko <arinamar7705@gmail.com>
2020-09-30 10:25:53 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  array.sort(compareNumbers = (a, b) => {
+    return a - b;
+  });
+  return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  array.sort(compareNumbers = (a, b) => {
+    return a - b;
+  });
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  let arrSum = 0;
+  array.map(item => arrSum += item);
+  return arrSum/array.length;
 }
